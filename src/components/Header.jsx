36616566a728bcd997174ab777028365df5893a3
Алена Егорова <egorovaal2915@mail.ru>
2026-02-13 17:21:56 +0900

    лаба2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <nav>
-      <div className="nav1">
+    <nav className="w-full h-[60px] flex flex-row items-center justify-center bg-[#E0FBFF]">
+      <div className="w-[50%] pl-[50px]">
         <Link to="/">
           <img width="200px" src="../public/img/logo1.png" alt="лого" />
         </Link>
-        ;
       </div>
-      <div className="nav2">
-        <Link to="/about">About</Link>;<Link to="/income">Income</Link>
-        <Link to="/expenses">Expenses</Link>
+      <div className="flex-row w-[50%] h-full pr-[50px] pl-[100px] bg-[#E0FBFF] flex flex-row ">
+        <Link to="/about" className="decoration-none text-[#ABC4FF] font-['Comic Sans MS'] text-[24px] font-bold m-auto">About</Link>
+        <Link to="/income" className="decoration-none text-[#ABC4FF] font-['Comic Sans MS'] text-[24px] font-bold m-auto">Income</Link>
+        <Link to="/expenses" className="decoration-none text-[#ABC4FF] font-['Comic Sans MS'] text-[24px] font-bold m-auto">Expenses</Link>
       </div>
     </nav>
   );
